5e8aab6c317d8af68ffd2d961021088dceb3c566
Nick Taylor <69398223+ntay90@users.noreply.github.com>
2026-02-16 13:01:10 -0500

    Updated preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,6 @@
 /* @type { import('@storybook/html-vite').Preview ) */
-<<<<<<< HEAD
-import '../src/styles/base.css';
-=======
 
 import '../src/styles/base.css';
->>>>>>> preview_update
 
 const preview = {
   parameters: {
