ccddd194c8478c81d7513764cbef046061efb443
Nick Taylor <69398223+ntay90@users.noreply.github.com>
2026-04-15 12:21:22 -0400

    Added autocomplete functionality.

--- src/01-atoms/text-input/TextInput.stories.js
+++ src/01-atoms/text-input/TextInput.stories.js
@@ -1,20 +1,44 @@
-import { TextInput } from "./TextInput";
+import {
+	TextInput
+} from "./TextInput";
 export default {
-  title: 'Text Input',
-  component: 'text-input',
-  argTypes: {
-    label: {control: 'text'},
-    instructions: {control: 'text'},
-	  errors: {control: 'text'},
-},
-  args: {
-    label: 'Yale HomePage',
-    instructions: 'This is an example instruction.',
-	  errors: 'This is an example error'
-},
+	title: 'Text Input',
+	component: 'text-input',
+	argTypes: {
+		label: {
+			control: 'text'
+		},
+		required: {
+			control: 'boolean',
+			mapping: {
+				true: "required",
+			},
+		},
+		
+		autocomplete: {
+			control: 'text',
+		},
+		
+		placeholder: {
+			control: 'text',
+		},
+		
+		instructions: {
+			control: 'text'
+		},
+		errors: {
+			control: 'text'
+		},
+	},
+	args: {
+		label: 'Yale HomePage',
+		instructions: 'This is an example instruction.',
+		errors: 'This is an example error',
+		required: false,
+	},
 
-  render: (args) => 
-    `<text-input class="">
+	render: (args) =>
+		`<text-input class="${args.required}">
 		<span slot="label">${args.label}</span>
 		<span slot="instructions">This is an example instruction</span>
 		<span slot="errors">This is an error.</span>
@@ -22,13 +46,42 @@ export default {
 };
 
 export const Example = {
-  args: {
-  },
-
-  render: (args) =>
-    `<text-input class="">
+	args: {
+        errors: "",
+        label: "Name"
+    },
+	render:(args) =>
+		`<text-input placeholder="${args.placeholder}" autocomplete="${args.autocomplete}" class="${args.required}">
 		<span slot="label">${args.label}</span>
 		<span slot="instructions">${args.instructions}</span>
 		<span slot="errors">${args.errors}</span>
 	</text-input>`
+};
+
+export const WithError = {
+    args: {
+        label: "Yale HomePage",
+        instructions: "This is an example instruction.",
+        errors: "This has an error.",
+        required: false
+    },
+    render:args => `<text-input class="${args.required}">
+        <span slot="label">${args.label}</span>
+        <span slot="instructions">${args.instructions}</span>
+        <span slot="errors">${args.errors}</span>
+    </text-input>`
+};
+
+export const Required = {
+    args: {
+        label: "Yale HomePage",
+        instructions: "This is an example instruction.",
+        errors: "",
+        required: true
+    },
+    render:args => `<text-input class="${args.required}">
+        <span slot="label">${args.label}</span>
+        <span slot="instructions">${args.instructions}</span>
+        <span slot="errors">${args.errors}</span>
+    </text-input>`
 };
