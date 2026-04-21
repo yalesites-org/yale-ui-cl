aa117dc4ffdd1b1e7e99c9d2503b252ef24947c1
Nick Taylor <69398223+ntay90@users.noreply.github.com>
2026-04-21 13:40:59 -0400

    Made updateIcon function of text link private

--- src/01-atoms/text-link/Link.js
+++ src/01-atoms/text-link/Link.js
@@ -11,52 +11,64 @@ linkTemplate.innerHTML = `
 `;
 const currentURL = window.location.origin;
 
-export class TextLink extends HTMLElement { #shadow;
-  static get observedAttributes() {
-    return ["href", "class", "target"];
-  }
+export class TextLink extends HTMLElement {
+	#shadow;
+	static get observedAttributes() {
+		return ["href", "class", "target"];
+	}
 
-  constructor() {
-    super();
-    this.#shadow = this.attachShadow({ mode: 'closed' });
-    this.#shadow.adoptedStyleSheets = [baseSheet, sheet];
-	this.#shadow.appendChild(document.importNode(linkTemplate.content, true));
-	this.link = this.#shadow.querySelector("a");
-  }
+	constructor() {
+		super();
+		this.#shadow = this.attachShadow({
+			mode: 'closed'
+		});
+		this.#shadow.adoptedStyleSheets = [baseSheet, sheet];
+		this.#shadow.appendChild(document.importNode(linkTemplate.content, true));
+		this.link = this.#shadow.querySelector("a");
+	}
 
 
-  attributeChangedCallback(name, oldValue, newValue) {
-	if (oldValue === newValue) return;
-	if (name === "href") this.link.href = newValue;		
-	if (name === "class") this.link.classList.add("link--" + newValue);
-	if (name === "target") this.link.target = newValue;
-	this.updateIcon();	
-  }
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (oldValue === newValue) return;
+		if (name === "href") this.link.href = newValue;
+		if (name === "class") this.link.classList.add("link--" + newValue);
+		if (name === "target") this.link.target = newValue;
+		this.#updateIcon();
+	}
 
-  updateIcon() {
-	  const icon = this.#shadow.querySelector(".fa-icon");
-	  const extension = this.link.href.split(".").pop().toLowerCase();
-	  const downloadExts = [ 'pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'zip', 'csv', 'xml', 'rtf' ];
+	#updateIcon() {
+		const icon = this.#shadow.querySelector(".fa-icon");
+		const extension = this.link.href.split(".").pop().toLowerCase();
+		const downloadExts = ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'zip', 'csv', 'xml', 'rtf'];
 
-	  if (icon) icon.remove();
-	  if (downloadExts.includes(extension)) {
-	  		this.link.insertAdjacentHTML("beforeend", `
+		if (icon) icon.remove();
+		
+		if (downloadExts.includes(extension)) { // Download link
+			this.link.insertAdjacentHTML("beforeend", `
 	  		<span class="fa-icon fa-regular fa-circle-down"><span class="visually-hidden">(link is a download)</span></span>`);
-	  	} else if (this.link.origin != currentURL) {
-   	   this.link.insertAdjacentHTML("beforeend", `
+		} else if (this.link.origin != currentURL) { // External link
+			this.link.insertAdjacentHTML("beforeend", `
 		<span class="fa-icon fa-solid fa-arrow-up-right"><span class="visually-hidden">(link is external)</span></span>`);
-	} else if (this.link.target === "_blank") {
-		this.link.insertAdjacentHTML("beforeend", `
+		} else if (this.link.target === "_blank") { // New window link
+			this.link.insertAdjacentHTML("beforeend", `
 		<span class="fa-icon fa-solid fa-arrow-up-right-from-square"><span class="visually-hidden">(link opens in new window)</span></span>`);
-	}  else return;
-  }
+		} else return;
+	}
 
-  get href() { return this.getAttribute("href"); }
-  get target() { return this.getAttribute("target"); }
+	get href() {
+		return this.getAttribute("href");
+	}
+	get target() {
+		return this.getAttribute("target");
+	}
 
-  set href(value) { return this.setAttribute("href", value); }
-  set target(value) { return this.setAttribute("target", value); }
-  
+	set href(value) {
+		return this.setAttribute("href", value);
+	}
+	set target(value) {
+		return this.setAttribute("target", value);
+	}
+
 }
 
 
