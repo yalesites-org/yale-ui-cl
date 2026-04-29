622bc4da86141701e371e62fc77b2ee6921da2fe
Nick Taylor <69398223+ntay90@users.noreply.github.com>
2026-04-29 09:44:47 -0400

    Updated package.json. Added CTA add classes function. Added export to main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import { Cta } from "./01-atoms/cta/Cta.js";
 import { TextLink } from "./01-atoms/text-link/Link.js";
 import { TextInput } from "./01-atoms/text-input/TextInput.js";
 import { SelectInput } from "./01-atoms/select-input/SelectInput.js";
+export { Cta, TextLink, TextInput, SelectInput }
