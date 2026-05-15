8f26112b5ba5718ce9b28e827aff432a9d74b6dc
codeclancy <128457307+codeclancy@users.noreply.github.com>
2026-05-15 10:54:56 -0400

    Divider

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import { TextInput } from "./01-atoms/text-input/TextInput.js";
 import { Table } from "./01-atoms/table/Table.js";
 import { SelectInput } from "./01-atoms/select-input/SelectInput.js";
 import { List } from "./01-atoms/lists/Lists.js";
+import { Divider } from "./01-atoms/divider/Divider.js";
 export { Cta, TextLink, TextInput, SelectInput, List }
